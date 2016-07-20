250e40c7ad83a65bd45a9a4d41cdaae6a3541222
Frank Murphy <fpmurphy@mtu.edu>
2016-07-19 21:56:36 -0400

    Decrease total pawn count in revolution

--- static/js/rules/revolution.js
+++ static/js/rules/revolution.js
@@ -4,13 +4,17 @@ var Revolution = _.clone(BaseRules);
 Revolution.startingPieces = function(board, Piece) {
     var base = BaseRules.startingPieces(board, Piece);
 
-    white = _.filter(base, function(p) { return p.player != "black" });
+    var white = _.filter(base, function(p) { return p.player != "black" });
     white = _.filter(white, function(p) { return p.type != "p" });
 
     var black = _.map(_.range(4, 8), function(y) {
-        return _.map(_.range(0, 8), function(x) {
-            return new Piece(board, "black", "p", x, y);
-        });
+        var pawn = function(x) { return new Piece(board, "black", "p", x, y) };
+        if(y === 4) {
+            return _.map(_.range(2, 6), pawn);
+        }
+        else {
+            return _.map(_.range(0, 8), pawn);
+        }
     });
 
     return _.flatten([white, black]);
